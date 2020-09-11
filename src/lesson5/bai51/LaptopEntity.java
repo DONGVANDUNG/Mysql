aad163f167dcb1d5244c67afccd6dbaf9bf14325
van dung <vandung030701@gmail.com>
2020-09-11 08:13:19 +0700

    INSERT LAPTOP TO DATABASE

--- src/lesson5/bai51/LaptopEntity.java
+++ src/lesson5/bai51/LaptopEntity.java
@@ -1,31 +1,16 @@
 package lesson5.bai51;
 
-import lesson4.bai41.LaptopService;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LaptopEntity {
-    private int id;
     private String name;
     private String url;
     private String maker;
     private String type;
     private String ram;
-    private String cpu;
     private String ssd;
-    private String hdd;
-    private Long price;
-    private String Screen_resolution;
-    private String screen_size;
-    private int sold;
-    private Timestamp created_timestamp;
-    private Timestamp last_updated_timestamp;
-    private Long FromPrice;
-    private Long toPrice;
-    private String sort;
-    private String card;
     private Connection connnection;
 
     public LaptopEntity(String name, String url, String maker, String type, String ram, String ssd) {
@@ -39,14 +24,6 @@ public class LaptopEntity {
 
     public LaptopEntity(Connection connnection) {
         this.connnection = connnection;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -71,78 +48,7 @@ public class LaptopEntity {
 
     public void setSsd(String ssd) {
         this.ssd = ssd;
-    }
 
-    public String getHdd() {
-        return hdd;
-    }
-
-    public void setHdd(String hdd) {
-        this.hdd = hdd;
-    }
-
-    public Long getPrice() {
-        return price;
-    }
-
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-
-    public String getScreen_resolution() {
-        return Screen_resolution;
-    }
-
-    public void setScreen_resolution(String screen_resolution) {
-        Screen_resolution = screen_resolution;
-    }
-
-    public int getSold() {
-        return sold;
-    }
-
-    public void setSold(int sold) {
-        this.sold = sold;
-    }
-
-    public Timestamp getCreated_timestamp() {
-        return created_timestamp;
-    }
-
-    public void setCreated_timestamp(Timestamp created_timestamp) {
-        this.created_timestamp = created_timestamp;
-    }
-
-    public Timestamp getLast_updated_timestamp() {
-        return last_updated_timestamp;
-    }
-
-    public void setLast_updated_timestamp(Timestamp last_updated_timestamp) {
-        this.last_updated_timestamp = last_updated_timestamp;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public Long getFromPrice() {
-        return FromPrice;
-    }
-
-    public void setFromPrice(Long fromPrice) {
-        FromPrice = fromPrice;
-    }
-
-    public Long getToPrice() {
-        return toPrice;
-    }
-
-    public void setToPrice(Long toPrice) {
-        this.toPrice = toPrice;
     }
 
     public String getMaker() {
@@ -153,30 +59,6 @@ public class LaptopEntity {
         this.maker = maker;
     }
 
-    public String getScreen_size() {
-        return screen_size;
-    }
-
-    public void setScreen_size(String screen_size) {
-        this.screen_size = screen_size;
-    }
-
-    public String getRam() {
-        return ram;
-    }
-
-    public void setRam(String ram) {
-        this.ram = ram;
-    }
-
-    public String getCpu() {
-        return cpu;
-    }
-
-    public void setCpu(String cpu) {
-        this.cpu = cpu;
-    }
-
     public String getType() {
         return type;
     }
@@ -185,37 +67,12 @@ public class LaptopEntity {
         this.type = type;
     }
 
-    public String getCard() {
-        return card;
+    public String getRam() {
+        return ram;
     }
 
-    public void setCard(String card) {
-        this.card = card;
-    }
-
-    @Override
-    public String toString() {
-        return "lesson4.bai41.LaptopEntity{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", url='" + url + '\'' +
-                ", maker='" + maker + '\'' +
-                ", type='" + type + '\'' +
-                ", ram='" + ram + '\'' +
-                ", cpu='" + cpu + '\'' +
-                ", ssd='" + ssd + '\'' +
-                ", hdd='" + hdd + '\'' +
-                ", price=" + price +
-                ", Screen_resolution='" + Screen_resolution + '\'' +
-                ", screen_size='" + screen_size + '\'' +
-                ", sold=" + sold +
-                ", created_timestamp=" + created_timestamp +
-                ", last_updated_timestamp=" + last_updated_timestamp +
-                ", FromPrice=" + FromPrice +
-                ", toPrice=" + toPrice +
-                ", sort='" + sort + '\'' +
-                ", card='" + card + '\'' +
-                '}';
+    public void setRam(String ram) {
+        this.ram = ram;
     }
 
     public void InsertInfor(LaptopEntity laptop) {
@@ -248,4 +105,19 @@ public class LaptopEntity {
             throwables.printStackTrace();
         }
     }
+
+    @Override
+    public String toString() {
+        return "LaptopEntity{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", maker='" + maker + '\'' +
+                ", type='" + type + '\'' +
+                ", ram='" + ram + '\'' +
+                ", ssd='" + ssd + '\'' +
+                ", connnection=" + connnection +
+                '}';
+    }
 }
+
+
