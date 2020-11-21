42103bb8bbec59035e4282c18e9d848fd4da61c4
van dung <vandung030701@gmail.com>
2020-11-21 07:48:33 +0700

    Exercise 3

--- src/spring/bai12/Threading.java
+++ src/spring/bai12/Threading.java
@@ -2,25 +2,25 @@ package spring.bai12;
 
 public class Threading {
     public static void main(String[] args) {
-        MyThread thread1 = new MyThread();
+        MyThread thread1=new MyThread("ABC",3500);
         thread1.start();
-        while (true) {
-            System.out.println("DEF");
-            try {
-                Thread.sleep(2500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        MyThread thread2=new MyThread("DEF",2500);
+        thread2.start();
     }
 }
 class MyThread extends Thread {
+    String name;
+    int sleep;
+    MyThread(String name,int sleep){
+        this.name=name;
+        this.sleep=sleep;
+    }
     @Override
     public void run() {
         while (true) {
-            System.out.println("ABC");
+            System.out.println(name);
             try {
-                Thread.sleep(3500);
+                Thread.sleep(sleep);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
