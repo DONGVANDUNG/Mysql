5b98f8d9999e5436408a77540491a2a5cc81c9f4
van dung <vandung030701@gmail.com>
2020-11-27 10:48:48 +0700

    Exsercise 1.4

--- src/spring/bai14/Threading4.java
+++ src/spring/bai14/Threading4.java
@@ -1,23 +1,12 @@
 package spring.bai14;
 
-import com.sun.deploy.net.proxy.ProxyUnavailableException;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import static sun.misc.PostVMInitHook.run;
 
 public class Threading4 {
-       public static int count = 1;
+    public static String key = null;
+
     public static void main(String[] args) {
-       Thread1 thread1=new Thread1();
-       Thread2 thread2=new Thread2();
-        thread1.start();
-        thread2.start();
-    }
-    public synchronized void display() {
         Map<String, String> add = new HashMap<String, String>();
         add.put("Monday", "Thu 2");
         add.put("Tuesday", "Thu 3");
@@ -26,37 +15,15 @@ public class Threading4 {
         add.put("Friday", "Thu 6");
         add.put("Saturday", "Thu 7");
         add.put("Sunday", "Chu nhat");
-        for (String i:add.keySet()) {
-        if (count == 1) {
-            System.out.println(i);
-        }
-        if (count == 0) {
-            System.out.println(add.get(i));
-            }
-        }
-    }
-     static class Thread1 extends Thread {
-         Threading4 main = new Threading4();
-         @Override
-         public void run() {
-             while (true) {
-                     main.display();
-                     count = 1;
-                     try {
-                         Thread.sleep(1000);
-                     } catch (InterruptedException e) {
-                         e.printStackTrace();
-                     }
-                 }
-             }
-         }
-    static class Thread2 extends Thread{
-        Threading4 main=new Threading4();
-        @Override
-        public void run() {
-            while (true){
-                main.display();
-                count=0;
+        Thread thread2 = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (key != null) {
+                        String value = add.get(key);
+                        System.out.println(value);
+                        key = null;
+                    }
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -64,5 +31,30 @@ public class Threading4 {
                     }
                 }
             }
-        }
+        };
+        Thread thread1 = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (key == null) {
+                        int index = new Random().nextInt(7);
+                        Set keys = add.keySet();
+                        List<String> ki = new ArrayList<>(keys);
+                        key = ki.get(index);
+                        System.out.println(key);
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        thread1.start();
+        thread2.start();
     }
+}
+
+
+
